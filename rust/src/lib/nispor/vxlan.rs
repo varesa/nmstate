17753676e473b8e03868301045c7bc24074c6475
Esa Varemo <esa@kuivanto.fi>
2023-02-09 05:24:11 +0200

    Revert "Avoid leaking nispor VxlanInfo defaults"
    This reverts commit e6a2aa92cc06b9799aad0fe3871b82c6d09b1d7b.

--- rust/src/lib/nispor/vxlan.rs
+++ rust/src/lib/nispor/vxlan.rs
@@ -8,16 +8,8 @@ pub(crate) fn np_vxlan_to_nmstate(
 ) -> VxlanInterface {
     let vxlan_conf = np_iface.vxlan.as_ref().map(|np_vxlan_info| VxlanConfig {
         id: np_vxlan_info.vxlan_id,
-        base_iface: if np_vxlan_info.base_iface.is_empty() {
-            None
-        } else {
-            Some(np_vxlan_info.base_iface.clone())
-        },
-        learning: if np_vxlan_info.learning {
-            None
-        } else {
-            Some(false)
-        },
+        base_iface: Some(np_vxlan_info.base_iface.clone()),
+        learning: Some(np_vxlan_info.learning),
         local: std::net::IpAddr::from_str(np_vxlan_info.local.as_str()).ok(),
         remote: std::net::IpAddr::from_str(np_vxlan_info.remote.as_str()).ok(),
         dst_port: Some(np_vxlan_info.dst_port),
